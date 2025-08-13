7de213b588d5a7fa84e7e0f34795379d6a659373
Albert585 <nnnn565@163.com>
2025-08-13 08:56:45 +0800

    switch 2 lvgl9.3

--- src/main.c
+++ src/main.c
@@ -1,5 +1,29 @@
 #include "lvgl.h"
+static void lv_linux_disp_init(void)
+{
+    const char *device = getenv_default("LV_LINUX_FBDEV_DEVICE", "/dev/fb0");
+    lv_display_t * disp = lv_linux_fbdev_create();
 
-lv_display_t *disp = lv_linux_fbdev_create();
-lv_linux_fbdev_set_file(disp, "/dev/fb0");
-printf("display created");
+    lv_linux_fbdev_set_file(disp, device);
+}
+static void lv_linux_touch_init(){
+    lv_indev_t *touch = lv_evdev_create(LV_INDEV_TYPE_POINTER, "/dev/input/event0");
+    lv_indev_set_display(touch, disp);
+}
+
+int main(void)
+{
+    lv_init();
+
+    /*Linux display device init*/
+    lv_linux_disp_init();
+    lv_linux_touch_init();
+    
+
+    while(1) {
+        lv_timer_handler();
+        usleep(5000);
+    }
+
+    return 0;
+}
